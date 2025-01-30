96d0cbe00dc766bff7824eea6d47ab666f570086
Edward Marecos <emarecos@bu.edu>
2025-01-30 17:34:44 -0500

    started the js

---
+++ mp-1/projects/projects.js
@@ -0,0 +1,25 @@
+<script>
+    document.getElement
+    let val1;
+    let val2;
+
+    let answer;
+
+    const add;
+    const remove;
+    const multiply;
+    const divide;
+    const power;
+    const clear;
+
+    if (val1 != Nan and val2 != Nan) {}
+
+    if(answer < 0) {
+        document.getElementById("answer").innerHTML.style.color = "red";
+    } else {
+        document.getElementById("answer").innerHTML.style.color = "#22223b";
+    }
+
+}
+
+</script>
