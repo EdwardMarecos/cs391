013ca75677cc6256057ea67993ed583346bcf6c4
Ed M <emarecos@bu.edu>
2025-01-30 23:12:07 -0500

    made the javascript and added more dynamic sizing

--- mp-1/projects/projects.js
+++ mp-1/projects/projects.js
@@ -1,25 +1,55 @@
-<script>
-    document.getElement
-    let val1;
-    let val2;
-
-    let answer;
-
-    const add;
-    const remove;
-    const multiply;
-    const divide;
-    const power;
-    const clear;
-
-    if (val1 != Nan and val2 != Nan) {}
-
-    if(answer < 0) {
-        document.getElementById("answer").innerHTML.style.color = "red";
-    } else {
-        document.getElementById("answer").innerHTML.style.color = "#22223b";
-    }
-
+function add() {
+    let val1=document.getElementById("val1").value;
+    let val2=document.getElementById("val2").value;
+    let output=Number(val1)+Number(val2);
+    aboveOrBelow(output);
 }
 
-</script>
+function subtract() {
+    let val1=document.getElementById("val1").value;
+    let val2=document.getElementById("val2").value;
+    let output=Number(val1)-Number(val2);
+    aboveOrBelow(output);
+}
+
+function multiply() {
+    let val1=document.getElementById("val1").value;
+    let val2=document.getElementById("val2").value;
+    let output=Number(val1)*Number(val2);
+    aboveOrBelow(output);
+}
+
+function divide() {
+    let val1=document.getElementById("val1").value;
+    let val2=document.getElementById("val2").value;
+    let output=Number(val1)/Number(val2);
+    aboveOrBelow(output);
+}
+
+function power() {
+    let val1=document.getElementById("val1").value;
+    let val2=document.getElementById("val2").value;
+
+    let output=1;
+    for(let i=0;i<val2;i++){
+        output=val1*output;
+    }
+
+    aboveOrBelow(output);
+}
+
+function aboveOrBelow(output) {
+    if (output<0){
+        document.getElementById("output-val").style.color="red";
+        document.getElementById("output-val").innerHTML=String(output);
+    } else {
+        document.getElementById("output-val").style.color="#22223b";
+        document.getElementById("output-val").innerHTML=String(output);
+    }
+}
+
+function reset() {
+    document.getElementById("val1").value="";
+    document.getElementById("val2").value="";
+    document.getElementById("output-val").innerHTML="";
+}
